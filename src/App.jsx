47bb849b51e04359a226b5c6325b29d0fc997c79
Neha Upadhyay <nehau072@gmail.com>
2026-08-04 23:06:09 +0530

    Intial react + vite + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Trading Risk Dashboard
+      </h1>
+    </div>
+  );
+}
+
+export default App;
